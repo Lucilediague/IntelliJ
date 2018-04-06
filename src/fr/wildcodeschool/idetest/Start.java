6fa97c76aeefd9f6028c77496dc38635f75f0388
Lucile Diague <lucile.diague0@orange.fr>
2018-04-06 11:16:03 +0200

    Initial commit

---
+++ src/fr/wildcodeschool/idetest/Start.java
@@ -0,0 +1,18 @@
+package fr.wildcodeschool.idetest;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
+
+public class Start {
+
+    public static void main(String... args) throws IOException {
+        System.out.println(ProcessHandle.current().pid());
+
+        JFrame jf = new JFrame("Gotcha!");
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.add(new JLabel(new ImageIcon(new URL("https://wildcodeschool.fr/wp-content/uploads/2017/01/deer.png"))));
+        jf.pack();
+        jf.setVisible(true);
+    }
+}
